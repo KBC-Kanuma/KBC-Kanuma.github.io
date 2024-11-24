395a3cf707f5bf8272529d5396532533f50f6016
KBC <kbc3220043@gmail.com>
2024-11-24 13:11:39 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+function isDarkMode() {
+	return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+const body = document.body;
+console.log(isDarkMode());
+if(isDarkMode() == true){
+	body.style.setProperty('--background_image', 'url(background_b.png)');
+	body.style.setProperty('--theme', '#000000');
+}else{
+	body.style.setProperty('--background_image', 'url(background.png)');
+	body.style.setProperty('--theme', '#ffffff');
+}
