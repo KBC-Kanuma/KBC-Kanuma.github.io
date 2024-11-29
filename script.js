968ea400a1fd0e34594a953a04aa49834b475802
KBC <kbc3220043@gmail.com>
2024-11-29 19:29:42 +0900

    Add files via upload
    Homeのべース案作成

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ function isDarkMode() {
 const body = document.body;
 console.log(isDarkMode());
 if(isDarkMode() == true){
-	body.style.setProperty('--background_image', 'url(background_b.png)');
 	body.style.setProperty('--theme', '#000000');
+	body.style.setProperty('--theme_b', '#888888');
+	body.style.setProperty('--theme_r', '#ffffff');
 }else{
-	body.style.setProperty('--background_image', 'url(background.png)');
 	body.style.setProperty('--theme', '#ffffff');
+	body.style.setProperty('--theme_b', '#aaaaaa');
+	body.style.setProperty('--theme_r', '#000000');
 }
